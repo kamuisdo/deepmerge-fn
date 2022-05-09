1e6dce0a9fa339ac058d83075ceadd9cdd4be7b0
doris <626314566@qq.com>
2022-05-09 18:03:04 +0800

    init

---
+++ dist/index.umd.js
@@ -0,0 +1,14 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["enum-helper"] = factory());
+})(this, (function () { 'use strict';
+
+	function sum(a, b) {
+	    return a + b;
+	}
+
+	return sum;
+
+}));
+//# sourceMappingURL=index.umd.js.map
